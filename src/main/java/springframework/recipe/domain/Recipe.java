d6e11d0d4a2fb0dcbad50773c0138f9e87e7f29d
Aigul <aigul.sibgatulli@mail.ru>
2021-11-26 23:52:22 +0300

    init sets

--- src/main/java/springframework/recipe/domain/Recipe.java
+++ src/main/java/springframework/recipe/domain/Recipe.java
@@ -13,6 +13,7 @@ import javax.persistence.Lob;
 import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
+import java.util.HashSet;
 import java.util.Set;
 
 @Entity
@@ -45,14 +46,14 @@ public class Recipe {
 
     // mappedBy_id = fk
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
-    private Set<Ingredient> ingredients;
+    private Set<Ingredient> ingredients = new HashSet<>();;
 
     @ManyToMany
     @JoinTable(name = "recipe_category",
             joinColumns = @JoinColumn(name = "recipe_id"),
             inverseJoinColumns = @JoinColumn(name = "category_id")
     )
-    private Set<Category> categories;
+    private Set<Category> categories = new HashSet<>();;
 
     public Long getId() {
         return id;
