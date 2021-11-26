275b315c90dfaf5dc76af707de2e3061d956a6ec
Aigul <aigul.sibgatulli@mail.ru>
2021-11-26 23:21:26 +0300

    query methods to find by desc

--- src/main/java/springframework/recipe/controllers/IndexController.java
+++ src/main/java/springframework/recipe/controllers/IndexController.java
@@ -2,13 +2,27 @@ package springframework.recipe.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import springframework.recipe.repositories.CategoryRepository;
+import springframework.recipe.repositories.UnitOfMeasureRepository;
 
 @Controller
 public class IndexController {
 
+    private final CategoryRepository categoryRepository;
+    private final UnitOfMeasureRepository unitOfMeasureRepository;
+
+    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+        this.categoryRepository = categoryRepository;
+        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    }
+
     @RequestMapping({"", "/", "/index", "/index.html"})
     public String getIndexPage() {
         System.out.println("TEST");
+        categoryRepository.findByDescription("Italian")
+                .ifPresent(category -> System.out.println("Category id: " + category.getId()));
+        unitOfMeasureRepository.findByDescription("Tablespoon")
+                .ifPresent(unitOfMeasure -> System.out.println("Unit of measure: " + unitOfMeasure.getId()));
         return "index";
     }
 }
