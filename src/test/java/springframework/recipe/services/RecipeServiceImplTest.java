0783b663404e5d5205fd0aa941b8dbe336f422f9
Aigul <aigul.sibgatulli@mail.ru>
2021-12-01 16:57:32 +0300

    test recipeservice with mockito

---
+++ src/test/java/springframework/recipe/services/RecipeServiceImplTest.java
@@ -0,0 +1,44 @@
+package springframework.recipe.services;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import springframework.recipe.domain.Recipe;
+import springframework.recipe.repositories.RecipeRepository;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+class RecipeServiceImplTest {
+
+    RecipeServiceImpl recipeServiceImpl;
+
+    @Mock
+    RecipeRepository recipeRepository;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        recipeServiceImpl = new RecipeServiceImpl(recipeRepository);
+    }
+
+    @Test
+    void getRecipes() {
+        Recipe recipe = new Recipe();
+        Set<Recipe> recipesData = new HashSet<>();
+        recipesData.add(recipe);
+
+        when(recipeRepository.findAll()).thenReturn(recipesData);
+
+        Set<Recipe> recipes = recipeServiceImpl.getRecipes();
+        assertEquals(recipes.size(), 1);
+
+        verify(recipeRepository, times(1)).findAll();
+    }
+}
