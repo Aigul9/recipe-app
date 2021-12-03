e7f3565f8bd2db688ae5087997d8a9a44e4f1c02
Aigul <aigul.sibgatulli@mail.ru>
2021-12-03 21:39:02 +0300

    remove redundant imports

--- src/main/java/springframework/recipe/domain/Category.java
+++ src/main/java/springframework/recipe/domain/Category.java
@@ -1,15 +1,11 @@
 package springframework.recipe.domain;
 
 import lombok.AllArgsConstructor;
-import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 import javax.persistence.ManyToMany;
 import java.util.HashSet;
 import java.util.Set;
