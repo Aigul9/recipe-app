6d8dfca5053c7f359e7b6b6174a9850665b8ced0
Aigul <aigul.sibgatulli@mail.ru>
2021-12-04 23:29:06 +0300

    add null check into setNotes

--- src/main/java/springframework/recipe/domain/Recipe.java
+++ src/main/java/springframework/recipe/domain/Recipe.java
@@ -60,11 +60,13 @@ public class Recipe extends BaseEntity {
             joinColumns = @JoinColumn(name = "recipe_id"),
             inverseJoinColumns = @JoinColumn(name = "category_id")
     )
-    private Set<Category> categories = new HashSet<>();;
+    private Set<Category> categories = new HashSet<>();
 
     public void setNotes(Notes notes) {
-        this.notes = notes;
-        notes.setRecipe(this);
+        if (notes != null) {
+            this.notes = notes;
+            notes.setRecipe(this);
+        }
     }
 
     public Recipe addIngredient(Ingredient ingredient) {
