8938336cb0aec4683a5352a6cdd6934a5e182a3f
Aigul <aigul.sibgatulli@mail.ru>
2021-12-04 23:29:56 +0300

    recipeForm, implement  /recipe/new, /recipe/show/id paths

--- src/main/java/springframework/recipe/controllers/RecipeController.java
+++ src/main/java/springframework/recipe/controllers/RecipeController.java
@@ -3,8 +3,11 @@ package springframework.recipe.controllers;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import springframework.recipe.commands.RecipeCommand;
 import springframework.recipe.services.RecipeService;
 
 @Slf4j
@@ -22,5 +25,18 @@ public class RecipeController {
         log.debug("Getting recipe page");
         model.addAttribute("recipe", recipeService.findById(Long.valueOf(id)));
         return "recipe/show";
+    }
+
+    @RequestMapping("recipe/new")
+    public String getRecipeForm(Model model) {
+        model.addAttribute("recipe", new RecipeCommand());
+        return "recipe/recipeForm";
+    }
+
+    @PostMapping
+    @RequestMapping("recipe") //, method = RequestMethod.POST)
+    public String saveOrUpdate(@ModelAttribute RecipeCommand command) {
+        RecipeCommand savedCommand = recipeService.saveRecipeCommand(command);
+        return "redirect:/recipe/show/" + savedCommand.getId();
     }
 }
