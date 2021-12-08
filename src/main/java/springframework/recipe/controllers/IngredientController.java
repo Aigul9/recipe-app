ff5ff842922a57882830af6f4f45e2fb892f9dfd
Aigul <aigul.sibgatulli@mail.ru>
2021-12-08 23:14:57 +0300

    fix ingredient controller test

--- src/main/java/springframework/recipe/controllers/IngredientController.java
+++ src/main/java/springframework/recipe/controllers/IngredientController.java
@@ -6,6 +6,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import springframework.recipe.commands.IngredientCommand;
 import springframework.recipe.commands.RecipeCommand;
@@ -79,7 +80,7 @@ public class IngredientController {
         return "recipe/ingredient/ingredientForm";
     }
 
-    @GetMapping("recipe/{recipeId}/ingredient")
+    @PostMapping("recipe/{recipeId}/ingredient")
     public String saveOrUpdate(@ModelAttribute IngredientCommand ingredientCommand) {
         IngredientCommand savedCommand = ingredientService.saveIngredientCommand(ingredientCommand);
         log.debug("Saved recipe id: " + savedCommand.getRecipeId());
--- src/test/java/springframework/recipe/controllers/ImageControllerTest.java
+++ src/test/java/springframework/recipe/controllers/ImageControllerTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.web.servlet.MockMvc;
