765ce98699c576156eec05501999b106dd8f9af1
Aigul <aigul.sibgatulli@mail.ru>
2022-02-06 00:16:27 +0300

    change tests to mysql db

--- src/test/java/springframework/recipe/repositories/UnitOfMeasureRepositoryIT.java
+++ src/test/java/springframework/recipe/repositories/UnitOfMeasureRepositoryIT.java
@@ -3,20 +3,25 @@ package springframework.recipe.repositories;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import springframework.recipe.domain.UnitOfMeasure;
 
+import javax.transaction.Transactional;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 @ExtendWith(SpringExtension.class)
+@ActiveProfiles("dev")
+@Transactional
+@AutoConfigureTestDatabase(replace=AutoConfigureTestDatabase.Replace.NONE)
 @DataJpaTest
-class UnitOfMeasureRepositoryIT {
+class UnitOfMeasureRepositoryTest {
 
     @Autowired
     UnitOfMeasureRepository unitOfMeasureRepository;
